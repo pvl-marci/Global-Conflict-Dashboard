bc435e78d748bdaed7144e12b26da309ba74cce0
pvl-marci <paulmarciniak2603@gmail.com>
2022-07-05 10:28:16 +0200

    maxqda

--- profiler.py
+++ profiler.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-df = pd.read_csv('datasets//testtable_two.csv', sep=';')
+df = pd.read_csv('datasets\owid_conflicts_gni.csv')
 
 df['year'] = pd.to_datetime(df['year'])
 
 
 profile = ProfileReport(df, title='Report')
-profile.to_file("test_two_report.html")
+profile.to_file("owid_gni_report.html")
